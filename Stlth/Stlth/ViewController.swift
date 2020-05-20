a478062d591a7c867bdde54e828e4c93725dfe46
Yinan <yinancode@gmail.com>
2020-05-20 13:06:39 -0700

    project created

---
+++ Stlth/Stlth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stlth
+//
+//  Created by Yinan Qiu on 5/20/20.
+//  Copyright © 2020 Yinan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
